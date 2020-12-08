30b9612398a4478003d8c2d82f99b4d7001c4675
chollma <53924848+chollma@users.noreply.github.com>
2020-12-07 21:37:00 -0800

    Building the footer component

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,6 +1,23 @@
 <template>
       <footer class="footer">
-       
+        <div class="footer__navigation">
+            <a class= "footer__link" href="#">Home</a>
+            <a class= "footer__link" href="#">Speaking</a>
+            <a class= "footer__link" href="#">Projects</a>
+            <a class= "footer__link" href="#">Blog</a>
+        </div>
+        <div>
+            Subscribe to emails from Cooper
+           <!-- <form>
+                <label>First Name</label>
+                <input type="text">
+                <label>Last Name</label>
+                <input type="text">
+                <label>Email</label>
+                <input type="text">
+                <button>Submit</button>
+            </form>-->
+        </div>
         <div class="footer__copyright">
           © 2020 Cooper Hollmaier
         </div>
@@ -21,6 +38,26 @@ export default {
 
 <style>
 
+.footer{
+    background-color: black;
+}
+.footer__navigation {
+    padding: 2rem;
+}
 
+.footer__link {
+    margin-right: 2rem;
+    color:white;
+    text-decoration: none;
+}
+
+.footer__copyright {
+    padding: 1rem;
+    color:white;
+}
+
+.footer__legal {
+    padding:1rem;
+}
 
 </style>
