cb4e14d199933197d9f4e4054309928e85ade622
chollma <53924848+chollma@users.noreply.github.com>
2020-12-30 17:52:17 -0800

    Locally hosting fonts

--- src/main.js
+++ src/main.js
@@ -1,18 +1,14 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
+require('typeface-vollkorn')
+require('typeface-lato')
 import '~/assets/scss/style.scss'
-
 import DefaultLayout from '~/layouts/Default.vue'
 import SiteHeader from '@/components/SiteHeader'
 import SiteFooter from '@/components/SiteFooter'
 
 
 export default function (Vue, { router, head, isClient }) {
-
-  head.link.push({
-    rel: 'stylesheet',
-    href: 'https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&family=Vollkorn:wght@400;500;700;800&display=swap'
-  })
   Vue.component('Layout', DefaultLayout)
   Vue.component('SiteHeader', SiteHeader)
   Vue.component('SiteFooter', SiteFooter)
